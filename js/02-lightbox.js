7f27955b3e2ad40d63bf7eebd05eaf834ca7e4b9
Alexander Vyzhymov <83233095+vyzhimov@users.noreply.github.com>
2023-01-23 01:23:52 +0200

    finished-homework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imageList = document.querySelector(".gallery");
+
+const itemsMarkup = galleryItems.reduce(
+  (acc, { original, preview, description }) => {
+    return (acc += `<a class='gallery__item' href="${original}">
+                    <img 
+                      class='gallery__image' 
+                      src="${preview}" 
+                      alt="${description}"">
+                    </img></a>`);
+  },
+  ""
+);
+
+imageList.insertAdjacentHTML("afterbegin", itemsMarkup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox", function () {});
